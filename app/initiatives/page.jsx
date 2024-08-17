61feae640feb771593988fe2f342a4a8ec5193d8
TheAdich <adidadhich2003@gmail.com>
2024-08-17 22:57:08 +0530

    fixed_button

--- app/initiatives/page.jsx
+++ app/initiatives/page.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 //import Image from "next/image";
 import "@splidejs/react-splide/css";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide,SplideTrack } from "@splidejs/react-splide";
 const images = [
     { src: "/pic.jpg", width: 600, height: 400 },
     { src: "/pic.jpg", width: 600, height: 400 },
@@ -25,6 +25,7 @@ const Carasouel = () => {
 
         <div className="w-full mx-auto my-12">
             <Splide
+                hasTrack={false}
                 options={{
                     type: "loop",
                     perPage: 1,
@@ -40,21 +41,34 @@ const Carasouel = () => {
 
                 aria-label="Image Carousel"
             >
-                {images.map((image, index) => (
-                    <SplideSlide key={index}>
-                        <div className="tablet:w-[52vw]  small_tablet:w-[65vw] mobile:w-[70vw] small_mobile:w-[80vw]  relative">
-                            <img
-                                src={image.src}
-                                alt=""
-                                className="pt-1 tablet:h-[40vh]  small_mobile:h-[20vh] small_mobile:w-[94%] mobile:h-[35vh] small_tablet:h-[30vh] tablet:w-[98%] mx-auto border-2 border-white border-dashed rounded-xl"
-                                style={{borderRadius:"30px"}}
-                            />
-                            <div>
-                                <p className="font-dm-sans font-normal small_mobile:w-[88%] tablet:w-[95%] mx-auto text-white pb-6">Alcheringa stands against the backdrop of communal disharmony plaguing India. Through AIKYAM, we aim to foster unity and peace. Expect thought-provoking panel discussions, soul-stirring performances, and impactful fundraisers. Join us in building a harmonious future.</p>
-                            </div>
+            <SplideTrack>
+            {images.map((image, index) => (
+                <SplideSlide key={index}>
+                    <div className="tablet:w-[52vw] small_tablet:w-[65vw] mobile:w-[70vw] small_mobile:w-[80vw] relative">
+                        <img
+                            src={image.src}
+                            alt=""
+                            className="pt-1 tablet:h-[40vh] small_mobile:h-[20vh] small_mobile:w-[94%] mobile:h-[35vh] small_tablet:h-[30vh] tablet:w-[98%] mx-auto border-2 border-white border-dashed rounded-xl"
+                            style={{ borderRadius: "30px" }}
+                        />
+                        <div>
+                            <p className="font-dm-sans font-normal small_mobile:w-[88%] tablet:w-[95%] mx-auto text-white pb-6">
+                                Alcheringa stands against the backdrop of communal disharmony plaguing India. Through AIKYAM, we aim to foster unity and peace. Expect thought-provoking panel discussions, soul-stirring performances, and impactful fundraisers. Join us in building a harmonious future.
+                            </p>
                         </div>
-                    </SplideSlide>
-                ))}
+                    </div>
+                </SplideSlide>
+            ))}
+        </SplideTrack>
+
+        <div className="splide__arrows">
+        <button className="splide__arrow splide__arrow--prev">
+            <img src="/leftarrow.jpg" alt="Previous" className="rounded-full"/>
+        </button>
+        <button className="splide__arrow splide__arrow--next" >
+            <img src="/rightarrow.jpg" alt="Next" className="rounded-full"/>
+        </button>
+    </div>
             </Splide>
         </div>
     </div>
